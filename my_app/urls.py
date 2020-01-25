1ed6f1b3b6375f252c71fee71ee0170f62dff1fb
unknown <dalmasogembo@gmail.com>
2020-01-25 18:47:11 +0300

    tenth commit

--- my_app/urls.py
+++ my_app/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('/', views.home, name='home'),
     path('new_Search', views.new_search, name='new_Search'),
-    path('add_todo/', views.add_todo, name='add_todo'),
+    path('/add_todo/', views.add_todo, name='add_todo'),
     path('delete_todo/<int:todo_id>/', views.delete_todo),
     # path('admin/', admin.site.urls),
 ]
