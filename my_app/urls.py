7901a630c29bf06b0d9a4425c808ec260cc8f9a6
unknown <dalmasogembo@gmail.com>
2020-01-25 19:13:26 +0300

    twlvt commit

--- my_app/urls.py
+++ my_app/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('new_Search', views.new_search, name='new_Search'),
-    path('add_todo', views.add_todo, name='add_todo'),
+    url(r'add_todo', views.add_todo, name='add_todo'),
     path('delete_todo/<int:todo_id>/', views.delete_todo),
     # path('admin/', admin.site.urls),
 ]
