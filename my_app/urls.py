920ba42fb6bb5661b62a05aa4e90d74fcbf7f96f
unknown <dalmasogembo@gmail.com>
2020-01-26 17:38:28 +0300

    This is a trial update on urls

--- my_app/urls.py
+++ my_app/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('new_Search', views.new_search, name='new_Search'),
     path('add_todo/', views.add_todo, name='add_todo'),
-    url(r'about_page/', views.about_page, name='about_page'),
-    url(r'add_todo_page/', views.add_todo_page, name='add_todo_page'),
+    path('/about_page/', views.about_page, name='about_page'),
+    path('/add_todo_page/', views.add_todo_page, name='add_todo_page'),
     path('delete_todo/<int:todo_id>/', views.delete_todo),
     # path('admin/', admin.site.urls),
 ]
